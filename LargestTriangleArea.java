62990ffb0bb37f480d80101059f4917159aa0a1a
Aditi Chandra <aditichandra126@gmail.com>
2026-03-29 22:43:44 +0530

    812. Largest Triangle Area

---
+++ LargestTriangleArea.java
@@ -0,0 +1,44 @@
+public class LargestTriangleArea {
+
+    public static double largestTriangleArea(int[][] p) {
+        double ans = 0;
+        int n = p.length;
+
+        // check all triplets
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    int x1 = p[i][0], y1 = p[i][1];
+                    int x2 = p[j][0], y2 = p[j][1];
+                    int x3 = p[k][0], y3 = p[k][1];
+
+                    // shoelace formula
+                    double area = 0.5 * Math.abs(
+                        x1 * (y2 - y3) +
+                        x2 * (y3 - y1) +
+                        x3 * (y1 - y2)
+                    );
+
+                    ans = Math.max(ans, area);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] points = {
+            {0, 0},
+            {0, 1},
+            {1, 0},
+            {0, 2},
+            {2, 0}
+        };
+
+        double result = largestTriangleArea(points);
+
+        System.out.println("Largest Triangle Area: " + result);
+    }
+}
